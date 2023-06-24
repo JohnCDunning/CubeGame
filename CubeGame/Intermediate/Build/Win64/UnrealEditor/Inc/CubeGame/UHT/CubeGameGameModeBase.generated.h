897bdade8f2378978142ef0d672c1008925eeda4
John Dunning <john.charles.dunning@gmail.com>
2023-06-24 21:07:16 +1000

    Project push 2

---
+++ CubeGame/Intermediate/Build/Win64/UnrealEditor/Inc/CubeGame/UHT/CubeGameGameModeBase.generated.h
@@ -0,0 +1,98 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "CubeGameGameModeBase.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CUBEGAME_CubeGameGameModeBase_generated_h
+#error "CubeGameGameModeBase.generated.h already included, missing '#pragma once' in CubeGameGameModeBase.h"
+#endif
+#define CUBEGAME_CubeGameGameModeBase_generated_h
+
+#define FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_SPARSE_DATA
+#define FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_RPC_WRAPPERS
+#define FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_ACCESSORS
+#define FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACubeGameGameModeBase(); \
+	friend struct Z_Construct_UClass_ACubeGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACubeGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/CubeGame"), NO_API) \
+	DECLARE_SERIALIZER(ACubeGameGameModeBase)
+
+
+#define FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesACubeGameGameModeBase(); \
+	friend struct Z_Construct_UClass_ACubeGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACubeGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/CubeGame"), NO_API) \
+	DECLARE_SERIALIZER(ACubeGameGameModeBase)
+
+
+#define FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACubeGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACubeGameGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACubeGameGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACubeGameGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACubeGameGameModeBase(ACubeGameGameModeBase&&); \
+	NO_API ACubeGameGameModeBase(const ACubeGameGameModeBase&); \
+public: \
+	NO_API virtual ~ACubeGameGameModeBase();
+
+
+#define FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACubeGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACubeGameGameModeBase(ACubeGameGameModeBase&&); \
+	NO_API ACubeGameGameModeBase(const ACubeGameGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACubeGameGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACubeGameGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACubeGameGameModeBase) \
+	NO_API virtual ~ACubeGameGameModeBase();
+
+
+#define FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_12_PROLOG
+#define FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_SPARSE_DATA \
+	FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_RPC_WRAPPERS \
+	FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_ACCESSORS \
+	FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_INCLASS \
+	FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_SPARSE_DATA \
+	FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_ACCESSORS \
+	FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> CUBEGAME_API UClass* StaticClass<class ACubeGameGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Unity_Projects_GitProjects_CubeGame_CubeGame_Source_CubeGame_CubeGameGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ CubeGame/Intermediate/Build/Win64/x64/CubeGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.36.32535
+#include "D:/Unity Projects/GitProjects/CubeGame/CubeGame/Intermediate/Build/Win64/x64/CubeGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.h"
