8370a0739fbe8bb261b321dbc6593b300572784c
John Dunning <john.charles.dunning@gmail.com>
2023-06-24 21:19:45 +1000

    Project push 3

--- CubeGame/Intermediate/Build/Win64/x64/CubeGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.36.32535
-#include "D:/Unity Projects/GitProjects/CubeGame/CubeGame/Intermediate/Build/Win64/x64/CubeGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.InclOrderUnreal5_1.h"
--- CubeGame/Source/CubeGame/CubeGame.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CubeGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CubeGame, "CubeGame" );
